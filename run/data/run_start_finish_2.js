e6783ac4860ce5958ecffc911cba79372676b483
theobarnhart <theodore.barnhart@colorado.edu>
2019-06-16 13:39:38 -0600

    initial

---
+++ run/data/run_start_finish_2.js
@@ -0,0 +1,9 @@
+var json_run_start_finish_2 = {
+"type": "FeatureCollection",
+"name": "run_start_finish_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "label": "Start" }, "geometry": { "type": "Point", "coordinates": [ -112.029473735950347, 46.599721310659845 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "label": "End" }, "geometry": { "type": "Point", "coordinates": [ -112.029310003836812, 46.59938381300185 ] } }
+]
+}
